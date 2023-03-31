16849962248c6269fadd486a01b7f4f334317bbd
Joseph Muto <jmuto2@gmail.com>
2023-03-30 22:26:41 -0400

    DS-5104 add new european countries to allowable, add HU to blacklisted

--- src/CountryListService.php
+++ src/CountryListService.php
@@ -9,7 +9,7 @@ class CountryListService
      */
     public function blacklistedCountries(): array
     {
-        return ['AL','BY','BA','BI','CF','HR','CU','KP','CD','IR','IQ','QZ','LA','LB','LY','MK','ME','NI','RS','SO','SS','SD','SY','UA','VE','YE','ZW','BS','BB','BJ','BF','KH','KY','TD','CO','KM','CG','DO','GQ','ER','GN','GW','HT','LS','LR','MG','MM','PA','UG','PR'];
+        return ['AL','BY','BA','BI','CF','HR','HU','CU','KP','CD','IR','IQ','QZ','LA','LB','LY','MK','ME','NI','RS','SO','SS','SD','SY','UA','VE','YE','ZW','BS','BB','BJ','BF','KH','KY','TD','CO','KM','CG','DO','GQ','ER','GN','GW','HT','LS','LR','MG','MM','PA','UG','PR'];
     }
 
     /**
